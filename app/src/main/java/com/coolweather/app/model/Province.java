3fac5a9a8d509bff3581e51d9b6ba8a593fbab3c
Fuzm <fuzimin89@gmail.com>
2017-01-15 16:14:05 +0800

    add databse helper class, all table and model for table

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,36 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Fuzm on 2017/1/15 0015.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
